c6764f93318ece4929a883c9e0b4e50f785ea7e1
Brightnes <85318430+Brightnes@users.noreply.github.com>
2021-10-09 08:48:47 +0200

    Update astro_pi_21_22.py
    It's just a rough version of the program; it needs some fixing depending on the purpose of the experiment.

--- astro_pi_21_22.py
+++ astro_pi_21_22.py
@@ -58,6 +58,57 @@ def dark_or_shining():
     else:
         ISS_shadowing = 1
 
+def convert(angle):
+    """
+    Convert a `skyfield` Angle to an EXIF-appropriate
+    representation (rationals)
+    e.g. 98° 34' 58.7 to "98/1,34/1,587/10"
+
+    Return a tuple containing a boolean and the converted angle,
+    with the boolean indicating if the angle is negative.
+    """
+    sign, degrees, minutes, seconds = angle.signed_dms()
+    exif_angle = f'{degrees:.0f}/1,{minutes:.0f}/1,{seconds*10:.0f}/10'
+    return sign < 0, exif_angle
+
+def capture(camera, image):
+    """Use `camera` to capture an `image` file with lat/long EXIF data."""
+    point = ISS.coordinates()
+
+    # Convert the latitude and longitude to EXIF-appropriate representations
+    south, exif_latitude = convert(point.latitude)
+    west, exif_longitude = convert(point.longitude)
+
+    # Set the EXIF tags specifying the current location
+    camera.exif_tags['GPS.GPSLatitude'] = exif_latitude
+    camera.exif_tags['GPS.GPSLatitudeRef'] = "S" if south else "N"
+    camera.exif_tags['GPS.GPSLongitude'] = exif_longitude
+    camera.exif_tags['GPS.GPSLongitudeRef'] = "W" if west else "E"
+
+    # Capture the image
+    camera.capture(image)
+
+def doing_stuff(index):
+    try:
+        index +=1
+        logger.info(f"Loop number {index} started")
+        location = ISS.coordinates()
+        lat = location.latitude.degrees
+        long = location.longitude.degrees
+        row = (datetime.now(),lat, long, round(sense.temperature,4), round(sense.humidity,4))# Add entries if needed
+        add_csv_data(data_file, row)
+        camera.start_preview()
+        # Camera warm-up time
+        sleep(2)
+        camera.capture(f"{base_folder}/image_{index:04d}.jpg")# Check if photo numbering is ok.
+        camera.close()# I guess opening and closing the camera at each shot is better.
+        #(base_folder/"image.jpg").unlink() # Uncomment for life in space
+
+        sleep(60)
+    except Exception as e:
+        logger.error(f'{e.__class__.__name__}: {e})')
+
+
 # Object names definitions
 sense = SenseHat()
 camera = PiCamera()
@@ -76,24 +127,16 @@ camera.framerate = 15
 
 # Part of program to repeat in a loop over 3 hours of execution
 
-try:
-    logger.info(f"Loop number {i+1} started")
-    location = ISS.coordinates()
-    lat = location.latitude.degrees
-    long = location.longitude.degrees
-    row = (datetime.now(),lat, long, sense.temperature, sense.humidity)# Mind updating with roundings
-    add_csv_data(data_file, row)
-    camera.start_preview()
-    # Camera warm-up time
-    sleep(2)
-    camera.capture(f"{base_folder}/image.jpg")
-    #(base_folder/"image.jpg").unlink() # Uncomment for life in space
-
-    
-    sleep(60)
-except Exception as e:
-    logger.error(f'{e.__class__.__name__}: {e})')
-
-
-i +=1 # Update loop numbering
+# Create a `datetime` variable to store the start time
+start_time = datetime.now()
+# Create a `datetime` variable to store the current time
+# (these will be almost the same at the start)
+now_time = datetime.now()
+# Run a loop for 2 minutes
+while (now_time < start_time + timedelta(minutes=174)):# properly edit timedelta value
+    doing_stuff(i)
+    sleep(1)
+    # Update the current time
+    now_time = datetime.now()
+    i +=1 # Update loop numbering
 
